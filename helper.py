cc77857907e8cc407e4669cbbcd2a8c6960beb05
Alck2 <cmleungal@connect.ust.hk>
2023-05-31 10:24:46 +0800

    added

--- helper.py
+++ helper.py
@@ -39,17 +39,17 @@ def draw_3d(x,y,z, xAxisTitle, yAxisTitle, zAxisTitle):
     ax.set_zlabel(zAxisTitle)
     plt.show()
 
-# Input:
-#   x: 1d numpy array
-#   snr: double
-# Output: 1d numpy array with same size as x
-# Given signal x, returns the signal with Additive Gaussian White Noise of Signal-to-Noise Ration snr.
-def awgn(x,snr):
-    snr = 10 ** (snr / 10.0)
-    xpower = np.sum(x**2) / len(x)
-    npower = xpower / snr
-    noise = np.random.randn(len(x)) * np.sqrt(npower)
-    return x+noise
+# # Input:
+# #   x: 1d numpy array
+# #   snr: double
+# # Output: 1d numpy array with same size as x
+# # Given signal x, returns the signal with Additive Gaussian White Noise of Signal-to-Noise Ration snr.
+# def awgn(x,snr):
+#     snr = 10 ** (snr / 10.0)
+#     xpower = np.sum(x**2) / len(x)
+#     npower = xpower / snr
+#     noise = np.random.randn(len(x)) * np.sqrt(npower)
+#     return x+noise
 
 # Input:
 #   estimates: arrayType
@@ -73,3 +73,30 @@ def findNMaxPeaks(y ,list, n):
     sortedPeaks = np.argsort(y[list])[::-1]
     return sortedPeaks[0:n]
 
+
+from numpy import sum,isrealobj,sqrt
+from numpy.random import standard_normal
+def awgn(s,SNRdB,L=1):
+  """
+    AWGN channel
+    Add AWGN noise to input signal. The function adds AWGN noise vector to signal 's' to generate a resulting signal vector 'r' of specified SNR in dB. It also
+    returns the noise vector 'n' that is added to the signal 's' and the power spectral density N0 of noise added
+    Parameters:
+        s : input/transmitted signal vector
+        SNRdB : desired signal to noise ratio (expressed in dB) for the received signal
+        L : oversampling factor (applicable for waveform simulation) default L = 1.
+    Returns:
+        r : received signal vector (r=s+n)
+  """
+  gamma = 10**(SNRdB/10) #SNR to linear scale
+  if s.ndim==1:# if s is single dimensional vector
+      P=L*sum(abs(s)**2)/len(s) #Actual power in the vector
+  else: # multi-dimensional signals like MFSK
+      P=L*sum(sum(abs(s)**2))/len(s) # if s is a matrix [MxN]
+  N0=P/gamma # Find the noise spectral density
+  if isrealobj(s):# check if input is real/complex object type
+      n = sqrt(N0/2)*standard_normal(s.shape) # computed noise
+  else:
+      n = sqrt(N0/2)*(standard_normal(s.shape)+1j*standard_normal(s.shape))
+  r = s + n # received signal
+  return r
